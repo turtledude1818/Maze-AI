bd2f4eb48102762c3e9ef63245122d6f19ae095f
Ryan Butler <awesometurtledude1@gmail.com>
2020-07-24 09:22:17 -0400

    Maze Drawing

--- classes.py
+++ classes.py
@@ -1,26 +1,28 @@
 from enum import Enum
 import pygame
 
-length = 40
+SIZE = 5
+WINDOW_SIZE = 800
+LENGTH = WINDOW_SIZE // SIZE
 
 class Walls(Enum):
     LEFT = 0
     RIGHT = 1
     TOP = 2
     BOTTOM = 3
+
 class WallPosition(Enum):
-    LEFT = ((0, 0), (0, length))
-    RIGHT = ((length, length), (length, 0))
-    TOP = ((length, 0), (0, 0))
-    BOTTOM = ((0, length), (length, length))
+    LEFT = ((0, 0), (0, LENGTH))
+    RIGHT = ((LENGTH, LENGTH), (LENGTH, 0))
+    TOP = ((LENGTH, 0), (0, 0))
+    BOTTOM = ((0, LENGTH), (LENGTH, LENGTH))
 
 class Block():
     def __init__(self, walls):
         self.walls = walls
-        self.surface = pygame.Surface((length, length))
+        self.surface = pygame.Surface((LENGTH, LENGTH))
         self.surface.fill((255, 255, 255))
         self.draw()
-
     def get_wall(self, location):
         if location == Walls.LEFT.value:
             return WallPosition.LEFT
@@ -29,23 +31,66 @@ class Block():
         if location == Walls.TOP.value:
             return WallPosition.TOP
         return WallPosition.BOTTOM
-
     def draw(self):
         self.surface.fill((255, 255, 255))
         for location, wall in enumerate(self.walls):
             if wall:
                 points = self.get_wall(location).value
-                pygame.draw.line(self.surface, (0, 0, 0), points[0], points[1], 5)
-
+                pygame.draw.line(self.surface, (0, 0, 0), points[0], points[1], LENGTH//10)
     def get_surf(self):
         return self.surface
+    def draw_wall(self, pos, place):
+        if pos[0] <= LENGTH // 10:
+            if place % SIZE == 0:
+                return None
+            self.walls[Walls.LEFT.value] = not self.walls[Walls.LEFT.value]
+            self.draw()
+            return Walls.LEFT
+        if pos[0] >= LENGTH - (LENGTH  // 10):
+            if place % SIZE == 39:
+                return None
+            self.walls[Walls.RIGHT.value] = not self.walls[Walls.RIGHT.value]
+            self.draw()
+            return Walls.RIGHT
+        if pos[1] <= LENGTH // 10:
+            if place // SIZE == 0:
+                return None
+            self.walls[Walls.TOP.value] = not self.walls[Walls.TOP.value]
+            self.draw()
+            return Walls.TOP
+        if pos[1] >= LENGTH - (LENGTH  // 10):
+            if place // SIZE == 39:
+                return None
+            self.walls[Walls.BOTTOM.value] = not self.walls[Walls.BOTTOM.value]
+            self.draw()
+            return Walls.BOTTOM
+        return None
 
 class Maze():
     def __init__(self, blocks, size):
         self.blocks = blocks
         self.size = size
-        self.surface = pygame.Surface((size*40, size*40))
+        self.surface = pygame.Surface((WINDOW_SIZE, WINDOW_SIZE))
         self.draw()
     def draw(self):
+        self.surface.fill((255, 255, 255))
         for position, block in enumerate(self.blocks):
-            self.surface.blit(block.get_surf(), ((position % self.size) * 40, (position // self.size) * 40))
+            self.surface.blit(block.get_surf(), ((position % self.size) * LENGTH, (position // self.size) * LENGTH))
+    def update_wall(self, pos):
+        absolute_pos = [i // 1 for i in pos]
+        pos = [(i / LENGTH) // 1 for i in pos]
+        place = int(pos[0] + (self.size * pos[1]))
+        wall = self.blocks[place].draw_wall([i % LENGTH for i in absolute_pos], place)
+        if wall == Walls.LEFT and place % self.size != 0:
+            self.blocks[place-1].walls[Walls.RIGHT.value] = not self.blocks[place-1].walls[Walls.RIGHT.value]
+            self.blocks[place-1].draw()
+        elif wall == Walls.RIGHT and place % self.size != self.size - 1:
+            self.blocks[place+1].walls[Walls.LEFT.value] = not self.blocks[place+1].walls[Walls.LEFT.value]
+            self.blocks[place+1].draw()
+        elif wall == Walls.TOP and place // self.size != 0:
+            self.blocks[place-self.size].walls[Walls.BOTTOM.value] = not self.blocks[place-self.size].walls[Walls.BOTTOM.value]
+            self.blocks[place-self.size].draw()
+        elif wall == Walls.BOTTOM and place // self.size != self.size - 1:
+            self.blocks[place+self.size].walls[Walls.TOP.value] = not self.blocks[place+self.size].walls[Walls.TOP.value]
+            self.blocks[place+self.size].draw()
+        self.draw()
--- main.py
+++ main.py
@@ -4,15 +4,13 @@ from classes import *
 
 pygame.init()
 clock = pygame.time.Clock()
-SIZE = 20
 BLOCKS = list()
 for x in range(SIZE*SIZE):
-    BLOCKS.append(Block((x % SIZE == 0, x % SIZE == SIZE-1, x // SIZE == 0, x // SIZE == SIZE - 1)))
+    BLOCKS.append(Block([x % SIZE == 0, x % SIZE == SIZE-1, x // SIZE == 0, x // SIZE == SIZE - 1]))
 MAZE = Maze(BLOCKS, SIZE)
 
 #MAZE = Maze((Block((True,False,True,False)),), 1)
-DISPLAYSURF = pygame.display.set_mode((800, 800))
-#DISPLAYSURF = pygame.display.set_mode((MAZE.size*length, MAZE.size*length))
+DISPLAYSURF = pygame.display.set_mode((WINDOW_SIZE, WINDOW_SIZE))
 
 pygame.display.set_caption("Maze")
 while True:
@@ -24,5 +22,7 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == MOUSEBUTTONDOWN:
+            MAZE.update_wall(event.pos)
     pygame.display.update()
     clock.tick(60)
