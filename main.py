aee6bb845b6529be2c3d62150755da4dfc46cd4a
Ryan Butler <awesometurtledude1@gmail.com>
2020-07-24 07:44:02 -0400

    Maze Outline

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ from classes import *
 
 pygame.init()
 clock = pygame.time.Clock()
+SIZE = 20
+BLOCKS = list()
+for x in range(SIZE*SIZE):
+    BLOCKS.append(Block((x % SIZE == 0, x % SIZE == SIZE-1, x // SIZE == 0, x // SIZE == SIZE - 1)))
+MAZE = Maze(BLOCKS, SIZE)
 
-MAZE = Maze((Block((True,False,True,False)),), 1)
+#MAZE = Maze((Block((True,False,True,False)),), 1)
 DISPLAYSURF = pygame.display.set_mode((800, 800))
 #DISPLAYSURF = pygame.display.set_mode((MAZE.size*length, MAZE.size*length))
+
 pygame.display.set_caption("Maze")
 while True:
     DISPLAYSURF.fill((255, 255, 255))
